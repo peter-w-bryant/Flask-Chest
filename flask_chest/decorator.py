69524f658660f124c838b2c34b9e528eaff59dfd
Peter Bryant <72423203+peter-w-bryant@users.noreply.github.com>
2024-01-04 15:27:02 -0600

    Clean decorator

--- flask_chest/decorator.py
+++ flask_chest/decorator.py
@@ -8,7 +8,6 @@ from flask import current_app, g, request
 
 from flask_chest import FlaskChest
 
-
 def flask_chest(
     chests: List[FlaskChest],
     tracked_vars: List[str],
@@ -21,29 +20,23 @@ def flask_chest(
             response = func(*args, **kwargs)
             write_tracked_variables(chests, tracked_vars)
             return response
-
         return wrapper
-
     return decorator
 
 def write_tracked_variables(chests: List[FlaskChest], tracked_vars: List[str]) -> None:
     # Write tracked variables to each chest
     for chest in chests:
-
         request_id = getattr(g, "custom_request_id", None)
-        
         # Write tracked variables to database
         for request_method, context_vars in tracked_vars.items():
             if request.method == request_method.upper():
-                
                 # For each global context var in the list of tracked vars
                 context_tuple_list = []
                 for var_name in context_vars:
                     if hasattr(g, var_name):
                         value = getattr(g, var_name)
                         context_tuple_list.append((var_name, value, request_id))
-                    
-                chest.write(context_tuple_list)
+                chest.write(context_tuple_list) # Generic write for all chest types
 
 def set_custom_request_id(request_id_generator):
     """
@@ -59,7 +52,6 @@ def set_custom_request_id(request_id_generator):
         g.custom_request_id = str(request_id_generator())
     else:
         g.custom_request_id = str(uuid.uuid4())
-
     # Check if the custom_request_id is too long
     if len(g.custom_request_id) > 255:
         g.custom_request_id = g.custom_request_id[:255]
